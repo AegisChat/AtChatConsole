3d1cbf3d02da4ac70c7030412d331323e719a85f
Aegis18 <rangeroverkill@gmail.com>
2017-07-30 14:33:30 -0400

    test

--- src/atchat/AtChat.java
+++ src/atchat/AtChat.java
@@ -18,8 +18,6 @@ public class AtChat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        User_Text_Message m = new User_Text_Message(null, null);
-        m.displayTime();
         try{
             File friends_list_file = new File("friends_list.txt");
             File blocked_list_file = new File("blocked_list.txt");
@@ -34,11 +32,11 @@ public class AtChat {
                 System.out.println("Blocked list file already exit");
             }
         User n = User.getInstance();
+            User_Text_Message m = new User_Text_Message("Hello", n.getID());
+            m.displayTime();
         
         }catch(IOException e){
             e.printStackTrace();
         }
     }
 }
-
-//mendel's edit
